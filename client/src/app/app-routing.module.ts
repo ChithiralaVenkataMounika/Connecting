393ce173b8f1162370eef84888b0054301e4deed
ChithiralaVenkataMounika <vchith1@lsu.edu>
2021-06-07 11:06:47 -0500

    routing addition

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { ListsComponent } from './lists/lists.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'members', component:MemberListComponent},
+  {path: 'members/:id', component:MemberDetailComponent},
+  {path: 'lists', component:ListsComponent},
+  {path: 'messages', component:MessagesComponent},
+  {path: '**', component:HomeComponent, pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
